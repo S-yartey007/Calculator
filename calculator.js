43bf9bf50b9a90626580bc785e6f052aabb72e0e
Emmanuel Yartey <supremeconquerorx@gmail.com>
2024-09-08 00:09:00 +0000

    interface implementation

---
+++ calculator.js
@@ -0,0 +1,26 @@
+function add(a,b) {
+    return a + b;
+}
+
+function subtract(a,b) {
+    return a - b;
+}
+
+function multiply(a,b) {
+    return a * b;
+}
+
+function divide(a,b) {
+    return a/b;
+}
+
+let operand1,operand2, operator;
+
+function operate(operator,a,b) {
+  if(operator === "+") return add(a,b);
+  else if(operator === "-") return subtract(a,b);
+  else if(operator === "*") return multiply(a,b);
+  else if(operator === "/") return divide(a,b);
+}
+
+console.log(operate("/",1,2));
